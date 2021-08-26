04a5fcc6db61b4c6452bdc1376305732beb7194a
Yoshifumi <86049189+Yoshifumi2918@users.noreply.github.com>
2021-08-26 17:58:56 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/yoshifumi/nishida/calcapp/MainActivity.kt
@@ -0,0 +1,68 @@
+package jp.techacademy.yoshifumi.nishida.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //idを取得
+        var et1: EditText = findViewById(R.id.et1)
+        var et2: EditText = findViewById(R.id.et2)
+        var button1: Button = findViewById(R.id.button1)
+        var button2: Button = findViewById(R.id.button2)
+        var button3: Button = findViewById(R.id.button3)
+        var button4: Button = findViewById(R.id.button4)
+
+        //ページ遷移
+        val intent = Intent(this, MainActivity2::class.java)
+
+
+        //四則計算をする
+        button1.setOnClickListener {
+
+            intent.putExtra("VALUE1",et1.text.toString().toFloat())
+
+            intent.putExtra("VALUE2",et2.text.toString().toFloat())
+
+            startActivity(intent)
+
+        }
+
+        button2.setOnClickListener {
+
+            intent.putExtra("VALUE3",et1.text.toString().toFloat())
+
+            intent.putExtra("VALUE4",et2.text.toString().toFloat())
+
+            startActivity(intent)
+        }
+
+        button3.setOnClickListener {
+
+            intent.putExtra("VALUE5",et1.text.toString().toFloat())
+
+            intent.putExtra("VALUE6",et2.text.toString().toFloat())
+
+            startActivity(intent)
+
+        }
+        button3.setOnClickListener {
+
+            intent.putExtra("VALUE7",et1.text.toString().toFloat())
+
+            intent.putExtra("VALUE8",et2.text.toString().toFloat())
+
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/jp/techacademy/yoshifumi/nishida/calcapp/MainActivity2.kt
@@ -0,0 +1,39 @@
+package jp.techacademy.yoshifumi.nishida.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+
+        //idを取得
+        var ans :TextView = findViewById(R.id.ans)
+        var back :Button = findViewById(R.id.back)
+
+            val value1 = intent.getFloatExtra("VALUE1", 0f)
+            val value2 = intent.getFloatExtra("VALUE2", 0f)
+            val value3 = intent.getFloatExtra("VALUE3", 0f)
+            val value4 = intent.getFloatExtra("VALUE4", 0f)
+            val value5 = intent.getFloatExtra("VALUE5", 0f)
+            val value6 = intent.getFloatExtra("VALUE6", 0f)
+            val value7 = intent.getFloatExtra("VALUE7", 0f)
+            val value8 = intent.getFloatExtra("VALUE8", 0f)
+
+            ans.text = "${value1 + value2}"
+
+
+            ans.text = "${value3 - value4}"
+
+
+            ans.text = "${value5 * value6}"
+
+
+            ans.text = "${value7 / value8}"
+        }
+    }
